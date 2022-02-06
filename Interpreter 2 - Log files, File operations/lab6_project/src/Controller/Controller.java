7e177ee82726790889884bc1d7c7605207dd03c0
917_Bogdan_Vlad <72063186+BVlad917@users.noreply.github.com>
2022-02-06 02:24:02 +0200

    Add files via upload

---
+++ Interpreter 2 - Log files, File operations/lab6_project/src/Controller/Controller.java
@@ -0,0 +1,69 @@
+package Controller;
+
+import Model.ADT.IStack;
+import Model.ADT.MyList;
+import Model.Exceptions.*;
+import Model.ProgramState.ProgramState;
+import Model.Statement.*;
+import Model.Value.IValue;
+import Repository.IRepository;
+
+import java.io.IOException;
+
+public class Controller {
+    private IRepository repo;
+    private boolean displayFlag;
+
+    public Controller(IRepository repo) {
+        this.repo = repo;
+        this.displayFlag = false;
+    }
+
+    public void addProgramState(ProgramState prg) {
+        this.repo.addProgramState(prg);
+    }
+
+    public ProgramState oneStep(ProgramState currentState) throws ControllerException, StackException, StatementException, ExpressionException, DictionaryException, FileException {
+        IStack<IStatement> executionStack = currentState.getExecutionStack();
+        if (executionStack.isEmpty()) {
+            throw new ControllerException("Program state's execution stack is empty.");
+        }
+        IStatement topStatement = executionStack.pop();
+        return topStatement.execute(currentState);
+    }
+
+    public void allSteps() throws ControllerException, StatementException, StackException, ExpressionException, DictionaryException, ListException, FileException, IOException {
+        ProgramState programState = this.repo.getCurrentProgramState();
+        if (programState.getExecutionStack().isEmpty()) {
+            throw new ControllerException("ERROR: The program was already executed. The execution stack is empty.");
+        }
+        this.repo.logPrgStateExec();
+        if (this.displayFlag) {
+            System.out.println("Program execution started:");
+            System.out.print(programState + "\n");
+        }
+        int outputListSize = 0;
+        MyList<IValue > output;
+        while (!programState.getExecutionStack().isEmpty()) {
+            this.oneStep(programState);
+            this.repo.logPrgStateExec();
+            if (this.displayFlag) {
+                System.out.println(programState);
+            } else {
+                output = programState.getOutput();
+                if (outputListSize != output.size()) {
+                    outputListSize = output.size();
+                    System.out.println(output.getElemAtIndex(output.size() - 1).toString());
+                }
+            }
+        }
+    }
+
+    void turnDisplayFlagOn() {
+        this.displayFlag = true;
+    }
+
+    void turnDisplayFlagOff() {
+        this.displayFlag = false;
+    }
+}
---
+++ Interpreter 2 - Log files, File operations/lab6_project/src/Model/ProgramState/ProgramState.java
@@ -0,0 +1,64 @@
+package Model.ProgramState;
+import Model.ADT.MyDictionary;
+import Model.ADT.MyList;
+import Model.ADT.MyStack;
+import Model.Statement.IStatement;
+import Model.Value.IValue;
+import Model.Value.StringValue;
+
+import java.io.BufferedReader;
+import java.nio.Buffer;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ProgramState {
+    private MyStack<IStatement> executionStack;
+    private MyDictionary<String, IValue> symbolTable;
+    private MyList<IValue> output;
+    private MyDictionary<StringValue, BufferedReader> fileTable;
+    private IStatement originalProgram;
+
+    public ProgramState(MyStack<IStatement> exeStack, MyDictionary<String, IValue> symTable, MyList<IValue> out, MyDictionary<StringValue, BufferedReader> fileT, IStatement origPrg) {
+        this.executionStack = exeStack;
+        this.symbolTable = symTable;
+        this.output = out;
+        this.fileTable = fileT;
+        this.originalProgram = origPrg.deepCopy();
+        this.executionStack.push(this.originalProgram);
+    }
+
+    public MyStack<IStatement> getExecutionStack() {
+        return this.executionStack;
+    }
+
+    public MyDictionary<String, IValue> getSymbolTable() {
+        return this.symbolTable;
+    }
+
+    public MyList<IValue> getOutput() {
+        return this.output;
+    }
+
+    public MyDictionary<StringValue, BufferedReader> getFileTable() { return this.fileTable; }
+
+    public IStatement getOriginalProgram() {
+        return this.originalProgram;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now) +
+               "\nExecution Stack:\n" +
+               this.executionStack.toString() +
+               "\n" +
+               "Symbol Table:\n" +
+               this.symbolTable.toString() +
+               "\n" +
+               "Output:\n" +
+               this.output.toString() +
+               "\n" +
+               "-".repeat(50);
+    }
+}
