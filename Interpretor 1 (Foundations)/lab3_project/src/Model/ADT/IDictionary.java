0e9a51a5318c8a5d0f1560f004eb4e3612f3acc5
917_Bogdan_Vlad <72063186+BVlad917@users.noreply.github.com>
2022-02-06 02:25:07 +0200

    Delete Interpretor 1 (Foundations) directory

--- Interpretor 1 (Foundations)/lab3_project/src/Model/ADT/IDictionary.java
+++
@@ -1,9 +0,0 @@
-package Model.ADT;
-
-import Model.Exceptions.DictionaryException;
-
-public interface IDictionary<K, V> {
-    void addKeyValuePair(K newKey, V newValue);
-    V lookUp(K key) throws DictionaryException;
-    boolean isDefined(K key);
-}
